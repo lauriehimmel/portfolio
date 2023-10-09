6c11337c77e30fb6a200ee9cf86394d94594589f
laurhimms <lauriehimmel1@gmail.com>
2023-10-09 18:37:43 -0500

    homepage updates

---
+++ src/components/Home/Home-header.jsx
@@ -0,0 +1,10 @@
+import "./Home-header.css";
+
+export default function HomeHeader() {
+  return (
+    <div className="home-header">
+      <p>Laurie</p>
+      <p>Himmel</p>
+    </div>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
-import './Home.css'
+import HomeHeader from "../../components/Home/Home-header";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="home-header">Laurie Himmel</h1>
+    <div className="home-body">
+      <HomeHeader />
     </div>
   );
 }
