a442aac2143ff5e6dce8906a05f76c7d0e9f4ada
laurhimms <lauriehimmel1@gmail.com>
2023-10-09 19:04:49 -0500

    about page updates

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,12 @@
 import './About.css'
 import Header from "../../components/Header/Header";
+import AboutBody from '../../components/About/About-body';
 
 export default function About () {
   return (
     <div>
       <Header />
+      <AboutBody />
     </div>
   )
 }
