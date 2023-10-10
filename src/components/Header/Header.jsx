3c4666a0289071b049f324c6a663589dcfa2d2c0
laurhimms <lauriehimmel1@gmail.com>
2023-10-09 19:17:07 -0500

    add header link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
-import './Header.css'
-export default function Header () {
+import "./Header.css";
+export default function Header() {
   return (
-    <div className="header"><p>LH</p></div>
-  )
+    <div className="header">
+      <a href='/'>LH</a>
+    </div>
+  );
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,12 +3,18 @@ import Kindr from "../../components/Projects/Kindr";
 import Mastermind from "../../components/Projects/Mastermind";
 import Wink from "../../components/Projects/Wink";
 
+import Header from "../../components/Header/Header";
+
+
 export default function Projects () {
   return (
+    <>
+    <Header />
     <div className="projects-main">
       <Kindr />
       <Wink />
       <Mastermind />
     </div>
+    </>
   )
 }
