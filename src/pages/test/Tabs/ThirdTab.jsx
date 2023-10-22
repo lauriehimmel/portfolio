f097f193ffce650074deb314c29ec262bd407da4
laurhimms <lauriehimmel1@gmail.com>
2023-10-21 21:22:01 -0400

    tab updates

---
+++ src/pages/test/Tabs/ThirdTab.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../test-tabs.css';
+
+const ThirdTab = () => {
+
+  return (
+    <div className="ThirdTab">
+      <img className="wink-logo" src="./assets/Wink-Logo.svg"/>
+      <p>Welcome to Wink, home of the cutest one-eyed animals you'll ever meet! Wink is a full-stack MERN app that allows you to create and interact with digital pets.</p>
+    </div>
+  );
+};
+export default ThirdTab;
--- src/pages/test/test-tabs.jsx
+++ src/pages/test/test-tabs.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./test-tabs.css";
 import FirstTab from "./Tabs/FirstTab";
 import SecondTab from "./Tabs/SecondTab";
+import ThirdTab from "./Tabs/ThirdTab";
 
 // https://blog.logrocket.com/how-to-build-tab-component-react/#creating-tab-component
 
@@ -16,19 +17,28 @@ const TestTabs = () => {
     setActiveTab("tab2");
   };
 
+  const handleTab3 = () => {
+    setActiveTab("tab3");
+  };
+
   return (
     <div className="Tabs">
-      {/* Tab nav */}
       <ul className="nav">
         <li
           className={activeTab === "tab1" ? "active" : ""}
           onClick={handleTab1}
         >
-          Tab 1
+          Wink
         </li>
         <li
           className={activeTab === "tab2" ? "active" : ""}
           onClick={handleTab2}
+        >
+          Tab 2
+        </li>
+        <li
+          className={activeTab === "tab3" ? "active" : ""}
+          onClick={handleTab3}
         >
           Tab 2
         </li>
