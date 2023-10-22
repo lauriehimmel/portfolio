0892a7709ff32302ffd0407f159d16891cf1f544
laurhimms <lauriehimmel1@gmail.com>
2023-10-21 22:23:07 -0400

    wink description

--- src/pages/test/Tabs/FirstTab.jsx
+++ src/pages/test/Tabs/FirstTab.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../test-tabs.css";
-import { useNavigate } from "react-router";
 
 const FirstTab = () => {
   return (
@@ -8,11 +7,17 @@ const FirstTab = () => {
       <a href="https://winkfriends.netlify.app/" target="blank">
         <img className="wink-logo" src="./assets/Wink-Logo.svg" />
       </a>
+      <div>
       <p>
         Welcome to Wink, home of the cutest one-eyed animals you'll ever meet!
-        Wink is a full-stack MERN app that allows you to create and interact
-        with digital pets.
       </p>
+      <br/>
+      <p>
+        Wink is a full-stack MERN app that allows you to create and interact with digital pets.
+      </p>
+      <br />
+      <p>Inspired by the 90s and my one-eyed dog Roux, it takes you back to a simpler time when phones had cords and apps were just what came before entrees.</p>
+      </div>
     </div>
   );
 };
