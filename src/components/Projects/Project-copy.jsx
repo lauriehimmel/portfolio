bf636d146c1e5250b5cb436fa179600ee0c5df13
laurhimms <lauriehimmel1@gmail.com>
2023-10-25 16:29:22 -0400

    update styling

--- src/components/Projects/Project-copy.jsx
+++ src/components/Projects/Project-copy.jsx
@@ -1,7 +1,13 @@
-export default function ProjectCopy(){
+export default function ProjectCopy() {
   return (
-    <div className="project-copy">
-      <p>Please enjoy a selection of my projects below. Questions? Thoughts? <a href="/contact">Drop me a line.</a> I'd love to hear from you!</p>
+    <div>
+      <p className="project-copy">
+        Please enjoy a selection of my projects below. Questions? Thoughts?{" "}
+        <a className="url" href="/contact">
+          Drop me a line.
+        </a>{" "}
+        I'd love to hear from you!
+      </p>
     </div>
-  )
+  );
 }
