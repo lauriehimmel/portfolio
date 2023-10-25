36855db2e082b0d0b40de5f8b6a236dc20aaa5f5
laurhimms <lauriehimmel1@gmail.com>
2023-10-25 16:20:33 -0400

    contact link

--- src/components/Projects/Project-copy.jsx
+++ src/components/Projects/Project-copy.jsx
@@ -1,7 +1,7 @@
 export default function ProjectCopy(){
   return (
     <div className="project-copy">
-      <p>Please enjoy a selection of my projects below. Questions? Thoughts? Drop me a line. I'd love to hear from you!</p>
+      <p>Please enjoy a selection of my projects below. Questions? Thoughts? <a href="/contact">Drop me a line.</a> I'd love to hear from you!</p>
     </div>
   )
 }
