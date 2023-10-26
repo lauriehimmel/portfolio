8886a8a70ba649295536233d621a9c5856978db4
laurhimms <lauriehimmel1@gmail.com>
2023-10-25 22:27:35 -0400

    updating react app to laurie himmel

--- src/components/Tabs/ThirdTab.jsx
+++ src/components/Tabs/ThirdTab.jsx
@@ -9,7 +9,7 @@ const ThirdTab = () => {
         <br />
         <p>
           {" "}
-          I'm currently taking the very first project, built with JavaScript, and turning it into a React app. I'm looking forward to seeing the growth I've made, and being able to execute on this project exactly the way I had originally envisioned.
+          I'm currently taking my very first project, built with JavaScript, and turning it into a React app. I'm looking forward to seeing the growth I've made, and being able to execute on this project exactly the way I had originally envisioned.
         </p>
         <br />
         <p><strong>Mastermind</strong> is usually a two-player game, but I wanted to create a version where the user plays against the computer to guess its randomly-generated code.</p>
