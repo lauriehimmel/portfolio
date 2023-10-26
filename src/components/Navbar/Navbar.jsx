3cdb18a88197d22f04c161f6cdf97461ee359ab0
laurhimms <lauriehimmel1@gmail.com>
2023-10-25 22:37:04 -0400

    update about colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,13 +40,13 @@ export default function Navbar() {
       >
         <ul>
           <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
             <a href="/projects">Projects</a>
           </li>
           <li>
             <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
           </li>
         </ul>
       </div>
