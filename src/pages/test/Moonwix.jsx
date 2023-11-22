087497c2bd89b631f499466d480346d909a91514
laurhimms <lauriehimmel1@gmail.com>
2023-11-22 18:09:14 -0500

    candle test page dark mode working

--- src/pages/test/Moonwix.jsx
+++ src/pages/test/Moonwix.jsx
@@ -1,20 +1,64 @@
-import './test.css'
-import cake from './cake.gif'
-import logo from './Moonwix.svg'
-import smoke from './cakesmoke.gif'
-import { useState } from 'react'
+import "./test.css";
+import cake from "./cake.gif";
+import logo from "./Moonwix.svg";
+import smoke from "./cakesmoke.gif";
+import disco from "./discoball.gif";
+import { useState } from "react";
 
-export default function Moonwix () {
-const [isLit, setIsLit] = useState(false)
-  function blowOut () {
-    isLit ? setIsLit(false) : setIsLit(true)
+export default function Moonwix() {
+  const [isLit, setIsLit] = useState(true);
+  function blowOut() {
+    isLit ? setIsLit(false) : setIsLit(true);
   }
-    return (
-        <div className="moonwix-bg">
-          <img className="moonwix-logo" src={logo} />
-          {isLit ? (
-          <img className="moonwix-cake" src={cake} onClick={blowOut}/>
-        ) : (<img className="moonwix-cake" src={smoke} onClick={blowOut}/>)}
+
+  const [isLit2, setIsLit2] = useState(true);
+  function blowOutTwo() {
+    isLit2 ? setIsLit2(false) : setIsLit2(true);
+  }
+
+  return (
+    <div className="moonwix-bg">
+      {/* <img className="moonwix-logo" src={logo} /> */}
+      {/* {isLit ? (
+        <img className="moonwix-cake" src={cake} onClick={blowOut} />
+        ) : (
+          <img className="moonwix-cake" src={smoke} onClick={blowOut} />
+        )} */}
+      {isLit2 ? (
+        <>
+        <div className="lightmode">
+          <div className="logocake">
+            <img className="moonwix-logo" src={logo} />
+            <img
+              className="moonwix-cake2"
+              src={smoke}
+              onClick={blowOutTwo}
+            />{" "}
+          </div>
         </div>
-    )
+        </>
+      ) : (
+        <div className="darkmode">
+          <div className="discoballz">
+            <img className="ballz" src={disco} />{" "}
+            <img className="ballz" src={disco} />{" "}
+            <img className="ballz" src={disco} />{" "}
+            <img className="ballz" src={disco} />{" "}
+            <img className="ballz" src={disco} />{" "}
+          </div>
+          <div className="logocake">
+            <img className="moonwix-logo" src={logo} />
+            <img
+              className="moonwix-cake2"
+              src={cake}
+              onClick={blowOutTwo}
+            />{" "}
+          </div>
+        </div>
+      )}
+      <div>
+        heyyy
+      </div>
+    </div>
+  );
 }
