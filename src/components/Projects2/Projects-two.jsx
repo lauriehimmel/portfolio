a0b4d62586014bb492640e6975aad9b5aa487c11
lauriehimmel <lauriehimmel1@gmail.com>
2023-12-08 12:15:08 -0500

    projects

--- src/components/Projects2/Projects-two.jsx
+++ src/components/Projects2/Projects-two.jsx
@@ -1,16 +1,31 @@
 import "./Projects-two.css";
 import Wink from "../../assets/Wink-Screenshot-2.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function ProjectsTwo() {
+  const [clicked, setClicked] = useState(false);
+  function onClick(e) {
+    if (e.target.innerText = "WINK")
+    clicked ? setClicked(false) : setClicked(true);
+  }
+
   return (
     <div className="project-titles">
       <div>Projects</div>
-      <a className="wink-project-title">WINK</a>
-      <a href="/Kindr"className="kindr-project-title">KINDR</a>
+      <div className="wink-project-title" onClick={onClick}>WINK</div>
+      {clicked ? <div>clicked</div> : <></>}
+      <div className="kindr-project-title">
+        KINDR
+      </div>
+      {clicked ? <div>clicked</div> : <></>}
       <div>Client Work</div>
-      <a href="/client-portfolio-1" className="dev-design-title">DEV PORTFOLIO</a>
-      <a href="/shine-with-sydney" className="sydney-site-title">SHINE WITH SYDNEY</a>
+      <a href="/client-portfolio-1" className="dev-design-title">
+        DEV PORTFOLIO
+      </a>
+      <a href="/shine-with-sydney" className="sydney-site-title">
+        SHINE WITH SYDNEY
+      </a>
     </div>
   );
 }
