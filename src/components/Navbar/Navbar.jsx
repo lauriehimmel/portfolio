c9a51115179f9169ef4c6ec2bf104206e34248e9
lauriehimmel <lauriehimmel1@gmail.com>
2023-12-20 14:47:07 -0500

    styling updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import "./Navbar.css";
 import hamburger from "../../assets/hamburger-menu.png";
 
-export default function Navbar({location}) {
+export default function Navbar({ location }) {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
   const heroRef = useRef(null);
 
@@ -23,61 +23,79 @@ export default function Navbar({location}) {
   return (
     <nav ref={heroRef} className="hero">
       <a href="/" className="logo">
-        LH
+        LAURIE <br /> HIMMEL
       </a>
       <div className="hover">
-      <img
-        src={hamburger}
-        className="hamburger"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded);
-        }}
-      />
+        <img
+          src={hamburger}
+          className="hamburger"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        />
       </div>
       <div
         className={
           isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
         }
       >
-        {location === "projects" &&
-        <ul className="nav-items">
-          <li>
-            <a className="location" href="/projects">Projects</a>
-          </li>
-          <li>
-            <a  className="notlocation" href="/contact">Contact</a>
-          </li>
-          <li>
-            <a  className="notlocation" href="/about">About</a>
-          </li>
-        </ul>
-}
-{location === "contact" &&
-        <ul className="nav-items">
-          <li>
-            <a className="notlocation" href="/projects">Projects</a>
-          </li>
-          <li>
-            <a  className="location" href="/contact">Contact</a>
-          </li>
-          <li>
-            <a  className="notlocation" href="/about">About</a>
-          </li>
-        </ul>
-}
-{location === "about" &&
-        <ul className="nav-items">
-          <li className="notLocation-li">
-            <a className="notlocation" href="/projects">Projects</a>
-          </li>
-          <li>
-            <a  className="notlocation" href="/contact">Contact</a>
-          </li>
-          <li>
-            <a  className="location" href="/about">About</a>
-          </li>
-        </ul>
-}
+        {location === "projects" && (
+          <ul className="nav-items">
+            <li>
+              <a className="location" href="/projects">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="notlocation" href="/contact">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a className="notlocation" href="/about">
+                About
+              </a>
+            </li>
+          </ul>
+        )}
+        {location === "contact" && (
+          <ul className="nav-items">
+            <li>
+              <a className="notlocation" href="/projects">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="location" href="/contact">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a className="notlocation" href="/about">
+                About
+              </a>
+            </li>
+          </ul>
+        )}
+        {location === "about" && (
+          <ul className="nav-items">
+            <li className="notLocation-li">
+              <a className="notlocation" href="/projects">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="notlocation" href="/contact">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a className="location" href="/about">
+                About
+              </a>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,12 +5,8 @@ import Laurie from "../../assets/Laurie.svg";
 
 export default function About({builtWith}) {
   return (
-    <div>
+
       <Navbar location={'about'}/>
-      <div className="about-content">
-        <AboutBody builtWith={builtWith} classname="about-body-paragraphs" />
-        <img className="about-img" src={Laurie} />
-      </div>
-    </div>
+
   );
 }
